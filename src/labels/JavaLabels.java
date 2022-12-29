f8b8055c98987b42344589edcec3b65f2597806f
Simon-Muchemi <muchemi.simon21@students.dkut.ac.ke>
2022-12-29 23:40:21 +0300

    Day 1: Labels

--- src/labels/JavaLabels.java
+++ src/labels/JavaLabels.java
@@ -5,16 +5,32 @@ import java.awt.*;
 
 public class JavaLabels {
     public static void main(String[] args){
-        var label = new JLabel();
-        label.setText("I am new here!!");
 
         var frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
         var image = new ImageIcon("coding.png");
+        var bgImage = new ImageIcon("photos.png");
+        var label = new JLabel();
+        var border = BorderFactory.createLineBorder(Color.green, 2);
+
+        label.setText("I'm new here!!");
+        label.setIcon(bgImage);
+        label.setHorizontalTextPosition(JLabel.CENTER);
+        label.setVerticalTextPosition(JLabel.TOP);
+        label.setForeground(new Color(20,202,222));
+        label.setFont(new Font("MV Boli", Font.ITALIC,50));
+        label.setIconTextGap(20);
+        label.setBackground(new Color(25,82,100));
+        label.setOpaque(true);
+        label.setBorder(border);
+        label.setVerticalAlignment(JLabel.CENTER);
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setBounds(0,0,250,250);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setIconImage(image.getImage());
-        frame.setSize(400,400);
         frame.getContentPane().setBackground(new Color(25,62,100));
         frame.setVisible(true);
+        frame.add(label);
+        frame.pack();// This should be done last
     }
 }
